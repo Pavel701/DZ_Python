29776bcccd1f99112cabd267504420e45e870f1d
flip701 <paulshipspb@gmail.com>
2023-06-28 15:15:38 +0300

    Добавлен файл main_4 с решение 4 ДЗ

--- main_3.py
+++ main_3.py
@@ -1,20 +1,19 @@
-n = int(input("Введите количество элементов в массиве: "))
-array = []
-for i in range(n):
-    num = int(input("Введите число: "))
-    array.append(num)
-x = int(input("Введите число X: "))
-
-# Вычисление количества вхождений числа X в массиве
-count = 0
-for num in array:
-    if num == x:
-        count += 1
-
-# Вывод результата
-print(f"Число {x} встречается в массиве: {count} раз.")
-#######################################################################################
-
+# n = int(input("Введите количество элементов в массиве: "))
+# array = []
+# for i in range(n):
+#     num = int(input("Введите число: "))
+#     array.append(num)
+# x = int(input("Введите число X: "))
+#
+# # Вычисление количества вхождений числа X в массиве
+# count = 0
+# for num in array:
+#     if num == x:
+#         count += 1
+#
+# # Вывод результата
+# print(f"Число {x} встречается в массиве: {count} раз.")
+################################################################################
 n = int(input("Введите количество элементов в массиве: "))
 array = []
 for _ in range(n):
@@ -27,9 +26,9 @@ closest = None
 min_diff = float('inf')
 for num in array:
     diff = abs(num - x)
-#     if diff < min_diff:
-#         min_diff = diff
-#         closest = num
-#
-# # Вывод результата
-# print(f"Самый близкий элемент к числу {x} в массиве: {closest}")
+    if diff < min_diff:
+        min_diff = diff
+        closest = num
+
+# Вывод результата
+print(f"Самый близкий элемент к числу {x} в массиве: {closest}")
