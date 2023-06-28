bed46894da1fa8f758e76dddfba281e90cb5ba3a
flip701 <paulshipspb@gmail.com>
2023-06-28 15:33:43 +0300

    Добавлен файл main_4 с решение 4 ДЗ Решение первой задачи Добавил решение 2 задачи

--- main_4.py
+++ main_4.py
@@ -1,4 +1,4 @@
-# Ввод количества элементов для двух множеств
+Ввод количества элементов для двух множеств
 n = int(input("Введите количество элементов первого множества: "))
 m = int(input("Введите количество элементов второго множества: "))
 
@@ -24,3 +24,23 @@ common = sorted(list(set1.intersection(set2)))
 print("Общие элементы, встречающиеся в обоих множествах, в порядке возрастания:")
 for element in common:
     print(element)
+
+#######################################################################################################################
+n = int(input("Введите количество кустов: "))
+
+bushes = []
+for i in range(n):
+    berries = int(input("Введите количество ягод на кусте {}: ".format(i+1)))
+    bushes.append(berries)
+
+max_berries = 0
+
+for i in range(n):
+    curr_bush = bushes[i]
+    left_bush = bushes[(i-1) % n]
+    right_bush = bushes[(i+1) % n]
+
+    total_berries = curr_bush + left_bush + right_bush
+    max_berries = max(max_berries, total_berries)
+
+print("Максимальное количество ягод:", max_berries)
